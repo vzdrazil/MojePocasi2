184cac6896388f2dd66ffc2daeed1f8fbc6fb9a2
Vít Zdražil <v_zdrazil@utb.cz>
2025-11-06 11:18:38 +0100

    UI design

--- app/src/main/java/com/example/mojepocasi/Activity/MainActivity.kt
+++ app/src/main/java/com/example/mojepocasi/Activity/MainActivity.kt
@@ -1,21 +1,64 @@
 package com.example.mojepocasi.Activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mojepocasi.ViewModel.WeatherViewModel
+import com.example.mojepocasi.databinding.ActivityMainBinding
+import com.example.mojepocasi.model.CurrentResponseApi
 import com.example.mojepocasi.ui.theme.MojePocasiTheme
+import retrofit2.Call
+import retrofit2.Response
+
 
 class MainActivity : ComponentActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val weatherViewModel: WeatherViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.apply{
+            addFlags(windowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor=Color.Transparent
+        }
+        binding.apply {
+            var lat=51.50
+            var lon=-0.12
+            var name="London"
+            CityText.text=name
+            progressBar.visibility=View.VISIBLE
+            weatherViewModel.loadCurrentWeather(lat,lon,"metric").enqueue(object:retrofit2.Callback<CurrentResponseApi>{
+                override fun onResponse(
+                    call: Call<CurrentResponseApi>,
+                    response: Response<CurrentResponseApi>
+                ) {
+                    if(response.isSuccessful){
+                        val data=response.body()
+                        progressBar.visibility= View.GONE
+                        detailLayout.visibility=View.VISIBLE
+                        data?.let{StatusText.text=it.weather?.get(0)?.main ?:"-"
+                        WindText.text=it.wind.speed.let{Math.round(it).toString()} }
+                    }
+                }
+
+                override fun onFailure(call: Call<CurrentResponseApi>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
         enableEdgeToEdge()
         setContent {
             MojePocasiTheme {
---
+++ app/src/main/java/com/example/mojepocasi/ViewModel/WeatherViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.mojepocasi.ViewModel
+import com.example.mojepocasi.Repository.WeatherRepository
+import androidx.lifecycle.ViewModel
+import com.example.mojepocasi.Server.ApiClient
+import com.example.mojepocasi.Server.ApiServices
+
+class WeatherViewModel(val repository:WeatherRepository): ViewModel()
+{
+    constructor():this(WeatherRepository(ApiClient().getClient().create(ApiServices::class.java)))
+    fun loadCurrentWeather(lat:Double,lng:Double,unit:String)=
+        repository.GetCurrentWeather(lat,lng,unit)
+}
